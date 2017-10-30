89fa0defb94c6b268a30a1045deef9c1bfb5b478
Dzmitry Skadretsov <Dzmitry_Skadretsov@epam.com>
2017-10-30 16:50:46 +0300

    first scala commit

--- src/main/java/com/git/learn/JavaGitTest.java
+++ src/main/java/com/git/learn/JavaGitTest.java
@@ -4,4 +4,9 @@ package com.git.learn;
  * Created by Dzmitry_Skadretsov on 10/30/2017.
  */
 public class JavaGitTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
 }
---
+++ src/main/scala/ScalaGitTest.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Dzmitry_Skadretsov on 10/30/2017.
+  */
+object ScalaGitTest extends App {
+
+  println("===!!!testing!!!===")
+
+}
